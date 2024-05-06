d1c3b445a1e48961042a920e7f1550a9d40495b3
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-05-06 11:05:29 +0200

    Added fetchRates file

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,22 @@
 import moment from "moment";
-import rates from "./services/currencyClient";
+import fetchRates from "./services/fetchRates";
 import twitterClient from "./services/twitterClient";
 
-async function postTweet(tweet: string) {
-  try {
-    await twitterClient.v2.tweet(tweet);
-  } catch (err) {
-    throw err;
-  }
-}
-const tweet = `
+fetchRates()
+  .then((response) => {
+    const rates = response.data.rates;
+
+    const tweet = `
     ${moment(Date.now()).locale("en-gb").format("llll")}
     \n💵 1 USD ⇛ R${1 / rates.USD}
     \n💷 1 GBP ⇛ R${1 / rates.GBP}
     \n💶 1 EUR ⇛ R${1 / rates.USD}
     \n💶 1 CAD ⇛ R${1 / rates.CAD}`;
 
-postTweet(tweet);
+    twitterClient.v2.tweet(tweet).catch((err) => {
+      console.log(err);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
